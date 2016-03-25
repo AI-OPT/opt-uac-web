80846e5bf498078525124fcf1cc56a438a2e6b62
jiaxiaoshuo <jiaxs@asiainfo.com>
2016-03-25 16:52:55 +0800

    发送邮件封装类

---
+++ src/main/java/com/ai/opt/uac/web/util/EmailUtil.java
@@ -0,0 +1,21 @@
+package com.ai.opt.uac.web.util;
+
+import com.ai.opt.sdk.mail.EmailFactory;
+import com.ai.opt.sdk.mail.EmailTemplateUtil;
+import com.ai.opt.uac.web.model.email.sendEmailRequest;
+
+public final class EmailUtil {
+
+	public static final String BIND_EMAIL = "email/template/uac-register-binemail.xml";
+	
+	private EmailUtil(){}
+	
+	public static void sendEmail(sendEmailRequest emailRequest) {
+		String htmlcontext = EmailTemplateUtil.buildHtmlTextFromTemplate(emailRequest.getTemplateRUL(), emailRequest.getData());
+		try {
+			EmailFactory.SendEmail(emailRequest.getTomails(), emailRequest.getCcmails(), emailRequest.getSubject(), htmlcontext);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
